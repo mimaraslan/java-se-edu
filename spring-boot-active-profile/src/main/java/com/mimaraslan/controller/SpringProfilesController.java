04462942beb28eff0b1a6ede65e2562d9d4fcf44
Mimar Aslan <mimaraslan@gmail.com>
2023-11-19 12:09:00 -0500

    Java calismasi
    Java projeleri calismasi

---
+++ spring-boot-active-profile/src/main/java/com/mimaraslan/controller/SpringProfilesController.java
@@ -0,0 +1,21 @@
+package com.mimaraslan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringProfilesController {
+
+	@Value("${property.myVal}")
+	private String profile;
+
+	@Autowired
+	private String myBean;
+
+	@GetMapping("/")
+	public String getActiveProfile() {
+		return "Active profile is :: " + profile + " <br/> <br/>Configured bean value is :: " + myBean;
+	}
+}
