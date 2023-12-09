bb885623a9743cde2daf5a151550deb0f7bf607d
Mimar Aslan <mimaraslan@gmail.com>
2023-12-09 11:05:38 -0500

    GROUP BY, HAVING, ORDER BY, INSERT, UPDATE, DELETE
    GROUP BY, HAVING, ORDER BY, INSERT, UPDATE, DELETE

--- Java_025_SQL/src/main/java/Oturum_02_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/Oturum2.sql
+++ Java_025_SQL/src/main/java/Oturum_02_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/Oturum2.sql
@@ -8,4 +8,20 @@ CREATE TABLE `mydemodb`.`customers` (
 
 
 
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Ali Yağız', 'Tar', '333', 'TR');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Mehmet Ali', 'Kaya', '333', 'CA');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Salih', 'Bilgin', '333', 'DE');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Ebru', 'Çınar', '444', 'DE');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Banu', 'Balta', '555', 'TR');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Ogün', 'Gültekin', '666', 'BR');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Ecem', 'Koro', '777', 'TR');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Kenan', 'Asker', '888', 'DE');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Mert', 'Kaynak', '999', 'NL');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Merve', 'Türk', '123', 'US');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Nevzat', 'Ağbaş', '774', 'US');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Şadiye', 'Hanım', '887', 'NL');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Süheda', 'Şen', '874', 'CA');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Yunus Emre', 'Karakaş', '812', 'BR');
+INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Erdinç', 'Özdemir', '435', 'TR');
+
 
