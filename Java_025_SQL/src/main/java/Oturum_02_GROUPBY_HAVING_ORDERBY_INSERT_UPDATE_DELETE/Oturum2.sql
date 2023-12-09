d5168ec7ea153e52c8593a267d557415802d9e8c
Mimar Aslan <mimaraslan@gmail.com>
2023-12-09 10:19:29 -0500

    GROUP BY, HAVING, ORDER BY, INSERT, UPDATE, DELETE
    GROUP BY, HAVING, ORDER BY, INSERT, UPDATE, DELETE

---
+++ Java_025_SQL/src/main/java/Oturum_02_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/Oturum2.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `mydemodb`.`customers` (
+  `customer_id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NOT NULL,
+  `last_name` VARCHAR(60) NOT NULL,
+  `phone` VARCHAR(35) NULL,
+  `country` VARCHAR(50) NULL,
+  PRIMARY KEY (`customer_id`));
+
+
+
+
