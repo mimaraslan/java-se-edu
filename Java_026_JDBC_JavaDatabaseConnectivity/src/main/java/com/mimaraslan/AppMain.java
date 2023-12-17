a0e5e869b98e2047882016a614001571f14a07e8
Mimar Aslan <mimaraslan@gmail.com>
2023-12-17 11:33:25 -0500

    JDBC
    JDBC

--- Java_026_JDBC_JavaDatabaseConnectivity/src/main/java/com/mimaraslan/AppMain.java
+++ Java_026_JDBC_JavaDatabaseConnectivity/src/main/java/com/mimaraslan/AppMain.java
@@ -2,6 +2,8 @@ package com.mimaraslan;
 
 public class AppMain {
     public static void main(String[] args) {
+
         System.out.println("Hello!");
+
     }
 }
--- Java_026_JDBC_JavaDatabaseConnectivity/src/main/java/com/mimaraslan/select/MySqlConnection.java
+++ Java_026_JDBC_JavaDatabaseConnectivity/src/main/java/com/mimaraslan/select/MySqlConnection.java
@@ -1,7 +1,48 @@
 package com.mimaraslan.select;
 
+import java.sql.*;
+
 public class MySqlConnection {
 
+    public static void main(String[] args) {
 
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            //  Class.forName("com.mysql.jdbc.Driver");
+            //  Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/companydb",
+                    "root",
+                    "123456789"
+            );
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM customers");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("customer_id") + " " +
+                        resultSet.getString("first_name") + " " +
+                        resultSet.getString("last_name"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Hata: " + e);
+            throw new RuntimeException(e);
+        } finally {
+            if (resultSet != null || statement != null || connection != null) {
+                try {
+                    resultSet.close();
+                    statement.close();
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+    }
 
 }
