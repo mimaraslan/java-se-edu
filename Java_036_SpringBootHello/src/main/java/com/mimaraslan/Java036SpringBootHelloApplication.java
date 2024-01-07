a0aca9dd9846913950b13dd6d848dc3b576c998f
Mimar Aslan <mimaraslan@gmail.com>
2024-01-07 14:02:24 -0500

    Spring Boot Hello

---
+++ Java_036_SpringBootHello/src/main/java/com/mimaraslan/Java036SpringBootHelloApplication.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java036SpringBootHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java036SpringBootHelloApplication.class, args);
+    }
+
+}
