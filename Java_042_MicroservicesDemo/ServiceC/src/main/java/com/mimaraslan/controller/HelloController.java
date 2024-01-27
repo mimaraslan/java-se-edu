06cc9f3eee0c53e3d0792fd2bdb3c4ab07b22a16
Mimar Aslan <mimaraslan@gmail.com>
2024-01-27 11:52:37 -0500

    Microservices
    Microservices

--- Java_042_MicroservicesDemo/ServiceC/src/main/java/com/mimaraslan/controller/HelloController.java
+++ Java_042_MicroservicesDemo/ServiceC/src/main/java/com/mimaraslan/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.mimaraslan.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- Java_043_MicroservicesProject/src/main/java/com/mimaraslan/Java043MicroservicesProjectApplication.java
+++ Java_043_MicroservicesProject/src/main/java/com/mimaraslan/Java043MicroservicesProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java042MicroservicesDemoApplication {
+public class Java043MicroservicesProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Java042MicroservicesDemoApplication.class, args);
+        SpringApplication.run(Java043MicroservicesProjectApplication.class, args);
     }
 
 }
