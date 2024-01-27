23a7f885d8288218a8a47eeb63f78bcadd2c5830
Mimar Aslan <mimaraslan@gmail.com>
2024-01-27 09:55:43 -0500

    Microservices
    Microservices

---
+++ Java_042_MicroservicesDemo/src/main/java/com/mimaraslan/Java042MicroservicesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java042MicroservicesDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java042MicroservicesDemoApplication.class, args);
+    }
+
+}
