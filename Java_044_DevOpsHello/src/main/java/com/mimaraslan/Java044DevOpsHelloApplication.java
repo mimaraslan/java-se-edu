16daea8d6bde21b4ddfad36f9214301f42215e7d
Mimar Aslan <mimaraslan@gmail.com>
2024-02-14 23:26:30 -0500

    DevOps
    DevOps

---
+++ Java_044_DevOpsHello/src/main/java/com/mimaraslan/Java044DevOpsHelloApplication.java
@@ -0,0 +1,13 @@
+package com.mimaraslan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java044DevOpsHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java044DevOpsHelloApplication.class, args);
+    }
+
+}
---
+++ Java_044_DevOpsHello/src/main/java/com/mimaraslan/controller/DevOpsController.java
@@ -0,0 +1,36 @@
+package com.mimaraslan.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+//   http://localhost:8080
+@RestController
+@RequestMapping
+public class DevOpsController {
+
+
+    //   http://localhost:8080
+    @GetMapping
+    public String devopsHello(){
+
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+
+        return "DevOps Hello: " + myObj;
+    }
+
+    //   http://localhost:8080/info
+    @GetMapping("/info")
+    public String devopsInfo(){
+
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+
+        return "DevOps Info: "+ myObj;
+    }
+
+}
