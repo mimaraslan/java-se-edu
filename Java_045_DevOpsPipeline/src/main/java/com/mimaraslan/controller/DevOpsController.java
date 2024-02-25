1e62230d5ec3501e921059e1b6f6810775cb2426
Mimar Aslan <mimaraslan@gmail.com>
2024-02-25 11:09:39 -0500

    DevOps Pipeline
    DevOps Pipeline

---
+++ Java_045_DevOpsPipeline/src/main/java/com/mimaraslan/controller/DevOpsController.java
@@ -0,0 +1,35 @@
+package com.mimaraslan.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+//   http://localhost:8080
+@RestController
+@RequestMapping
+public class DevOpsController {
+
+    //   http://localhost:8080
+    @GetMapping
+    public String devopsHello(){
+
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+
+        return "DevOps Docker Hello: " + myObj;
+    }
+
+    //   http://localhost:8080/info
+    @GetMapping("/info")
+    public String devopsInfo(){
+
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println(myObj);
+
+        return "DevOps Docker Info: "+ myObj;
+    }
+
+}
